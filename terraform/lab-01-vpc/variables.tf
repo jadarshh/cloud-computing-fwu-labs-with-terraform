af307dc22eafe0037db7b3fedf043b7d54cccd3b
jadarsh <joshiadarsh239@gmail.com>
2026-05-16 12:20:12 +0545

    Commit : Canged Till Lab4 For Adarsh

--- terraform/lab-01-vpc/variables.tf
+++ terraform/lab-01-vpc/variables.tf
@@ -15,7 +15,50 @@ variable "display_name" {
   default     = "Mukesh"
 }
 
+variable "full_name" {
+  type        = string
+  description = "Student full name used for lab documentation."
+  default     = "Mukesh Pant"
+}
+
+variable "roll_number" {
+  type        = string
+  description = "Student roll number used for lab documentation."
+  default     = "29"
+}
+
+variable "semester" {
+  type        = string
+  description = "Student semester used for lab documentation."
+  default     = "VIII"
+}
+
 variable "region" {
-  type    = string
-  default = "ap-south-1"
+  type        = string
+  description = "AWS region where lab resources are created."
+  default     = "ap-south-1"
+}
+
+variable "region_label" {
+  type        = string
+  description = "Human-readable AWS region name used for lab documentation."
+  default     = "Asia Pacific (Mumbai)"
+}
+
+variable "institution" {
+  type        = string
+  description = "Institution name used for lab documentation."
+  default     = "Far Western University"
+}
+
+variable "faculty" {
+  type        = string
+  description = "Faculty name used for lab documentation."
+  default     = "Faculty of Engineering"
+}
+
+variable "subject_name" {
+  type        = string
+  description = "Subject name used for lab documentation."
+  default     = "Cloud Computing"
 }
